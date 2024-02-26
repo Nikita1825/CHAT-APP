f6f9c54910815eddec0f6bdd670aaa0d847f423e
Nikita1825 <nikitapostnov1825@gmail.com>
2024-02-27 00:38:51 +0300

    Додав частину бекенда

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+import  express  from 'express';
+
+import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
+const app = express();
+
+
+dotenv.config()
+const PORT = process.env.PORT || 5000; 
+
+app.get("/", (req, res)=>{
+  res.send("Hello world!")  
+})
+
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, ()=> {
+  connectToMongoDB()
+   console.log(`Server Running on port ${PORT} `)
+  });
