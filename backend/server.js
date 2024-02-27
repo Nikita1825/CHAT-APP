be0989aa24262c24b44f9f2f6c6873fcadd95a03
Nikita1825 <nikitapostnov1825@gmail.com>
2024-02-27 14:12:18 +0300

    Додав схему

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ const app = express();
 dotenv.config()
 const PORT = process.env.PORT || 5000; 
 
-app.get("/", (req, res)=>{
-  res.send("Hello world!")  
-})
+// app.get("/", (req, res)=>{
+//   res.send("Hello world!")  
+// })
 
-
+app.use(express.json())
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, ()=> {
